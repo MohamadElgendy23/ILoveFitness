10c8b016ba7203a1a52d2cc20ce4a0250b55deab
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-01-18 11:04:53 -0700

    creating readme

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import "./App.css";
 
 function App() {
   const [exercises, setExercises] = useState([]);
-  return <div className="flex items-center flex-col justify-center"></div>;
+  return (
+    <div className="flex items-center flex-col justify-center gap-5 mt-5">
+      <h1 className="font-bold text-6xl">ILoveFitness</h1>
+      <p className="italic text-3xl">
+        This app displays various muscle building exercises for gym goers and
+        lovers of fitness!
+      </p>
+    </div>
+  );
 }
 
 export default App;
