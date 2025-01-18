c7363f4746b7f29a8651eb9d28cb603c93fdbf67
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-01-18 10:34:57 -0700

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [exercises, setExercises] = useState([]);
+  return <div className="flex items-center flex-col justify-center"></div>;
+}
+
+export default App;
